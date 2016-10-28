500a4de7a3bc59174f438d6f7c515544090446f9
ERNESTO <EGMM.566@GMAIL.COM>
2016-10-28 17:40:38 +0100

    Agregando la galeria y haciendo que tanto galeria como contacto sean  peticiones asincronas

--- responsive.js
+++ responsive.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	var windowSize = $(window).width();
-	var menuContent = "<div class=\"%clase%\" id=\"%id%\"></div>";
 	/*
 	* La function responsive se encarga de quitar clases no necesarias para ciertos tamaños de pantalla
 	*/
@@ -24,59 +23,22 @@ $(document).ready(function(){
 		}
 	};
 /*
-* HTML del formulario de contacto
-*/
-var contacto = {
-	//encabezado: "<h2 class=\"text-center\">CONTACTO</h2>",
-	form: "<div class=\"input-group contact\"></div>",
-	contenido: {
-		nombre: "<input id= \"cont-nombre\" type=\"text\" class=\"form-control\" placeholder=\"NOMBRE\" aria-describedby=\"basic-addon1\">",
-		correo: "<input id= \"cont-correo\" type=\"text\" class=\"form-control\" placeholder=\"CORREO\" aria-describedby=\"basic-addon1\">",
-		mensaje: "<textarea id= \"cont-mensaje\" type=\"text\" class=\"form-control\" placeholder=\"MENSAJE\" aria-describedby=\"basic-addon1\"></textarea>",
-		boton: "<button type=\"button\" class=\"btn btn-default\">ENVIAR</button>"
-	}
-};
-/*
-* HTML de la galeria
-*/
-var galeria = {
-	fotoParaMostrar: [
-		"<img src=\"img/erika2.jpg\">",
-		"<img src=\"img/gaby2.jpg\">",
-		"<img src=\"img/gaby5.jpg\">",
-		"<img src=\"img/erika1.jpg\">",
-		"<img src=\"img/foto3.jpg\">",
-		"<img src=\"img/gaby3.jpg\">"],
-	thumbnail: "<div class=\"thumbnail\"></div>",
-	columnas: "<div class=\"col-md-4\"></div>"
-}
- /*
 	* Esta funcion va a agregar el formulario de contacto
  */
- var createContact = function(){
-	 var menuContact = menuContent.replace("%id%", "contacto");
-	 $("#fotos").prepend(menuContact.replace("%clase%", "menu-contacto"));
-	 for(var i in contacto.contenido){
-		 $("#contacto").append(contacto.form);
-		 $("#contacto div").last().append(contacto.contenido[i]);
-	 }
+ var loadContact = function(){
+	 $.get( "contacto.html", function( data ) {
+			 $( "#fotos" ).prepend( data );
+		 });
  }
  /*
  * Esta funcion agrega la galeria
  */
- var createGaleria = function () {
-	var menuGaleria = menuContent.replace("%id%", "galeria");
-	$("#container").css("display", "none");
-	$("#fotos").addClass("container");
- 	$("#fotos").prepend(menuGaleria.replace("%clase%", "row"));
-	for(var i = 0; i < 3; i++){
-		$("#galeria").append(galeria.columnas);
-		for(var k = 0; k < 2; k++){
-			$("#galeria div").last().append(galeria.thumbnail);
-			$("#galeria div div").last().append(galeria.fotoParaMostrar[i]);
-		}
+	var loadGalery = function(){
+		$("#container").css("display", "none");
+		$.get( "galeria.html", function( data ) {
+				$( "#fotos" ).append( data );
+			});
 	}
- }
 	/*
 	* Esto controla la apertura del menu
 	*/
@@ -91,6 +53,7 @@ var galeria = {
 	*/
 	$("#logo").click(function(){
 		$("#contacto").remove();
+		$("#galeria").remove();
 		$("#container").css("display", "block");
 	});
 	/*
@@ -101,7 +64,7 @@ var galeria = {
 		if(this.textContent === "GALERÍA"){
 			if($("#galeria").length){
 			} else {
-				createGaleria();
+				loadGalery();
 			}
 		} else if(this.textContent === "INFO"){
 			if($("#info").length){
@@ -110,9 +73,8 @@ var galeria = {
 			$("#fotos").append(menuContent.replace(data, "info"));
 		} else if(this.textContent === "CONTACTO"){
 			if($("#contacto").length){
-				//$("#contacto").remove();
 			} else {
-				createContact();
+				loadContact();
 			}
 		}
 	});
